c6453eb16ad734dbf2587f772a78e63db8b24243
Larrea <gorkalarreamontoya@gmail.com>
2023-05-18 13:46:59 +0200

    Cambia un par de nombres para diferenciar el Fork de la original
    *cambiado el nombre del usuario en la BD
    *cambiado el nombre y version del proyecto en la POM

--- sql/create-ludofunDB.sql
+++ sql/create-ludofunDB.sql
@@ -1,11 +1,11 @@
 	/* DELETE 'LudoFunDB' database*/
 	DROP SCHEMA IF EXISTS LudoFunDB;
 	/* DELETE USER 'spq' AT LOCAL SERVER*/
-	DROP USER IF EXISTS 'spq'@'localhost';
+	DROP USER IF EXISTS 'gorka'@'localhost';
 
 	/* CREATE 'LudoFunDB' DATABASE */
 	CREATE SCHEMA LudoFunDB;
-	/* CREATE THE USER 'spq' AT LOCAL SERVER WITH PASSWORD 'Imanol*123' */
-	CREATE USER IF NOT EXISTS 'spq'@'localhost' IDENTIFIED BY 'Imanol*123';
+	/* CREATE THE USER 'gorka' AT LOCAL SERVER WITH PASSWORD 'fork' */
+	CREATE USER IF NOT EXISTS 'gorka'@'localhost' IDENTIFIED BY 'fork';
 
-	GRANT ALL ON LudoFunDB.* TO 'spq'@'localhost';
+	GRANT ALL ON LudoFunDB.* TO 'gorka'@'localhost';
